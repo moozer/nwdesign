160f842737773a045aa8e7167f7cd37957449145
moz <moz@local>
2015-07-08 12:14:24 +0200

    now parsing reSt text to extract deviceids and titles

--- nwdesign/restIO/restReader.py
+++ nwdesign/restIO/restReader.py
@@ -5,7 +5,9 @@ Created on Jul 7, 2015
 '''
 
 import docutils.parsers.rst
-import docutils.utils.new_document
+from docutils import utils
+from docutils import core
+
 
 class restReader(object):
     '''
@@ -18,19 +20,42 @@ class restReader(object):
         Constructor
         '''
         self._filename = filename
+        self._parseFile()
+        self._hwEntry = None
+        self._hwEntryIds = "hardware-devices"
         
     @property
     def filename(self):
         return self._filename
     
-    def parseFile(self):
+    def _parseFile(self):
         with open(self.filename) as f:
             parser = docutils.parsers.rst.Parser()
             settings = docutils.frontend.OptionParser(
                 components=(docutils.parsers.rst.Parser,)
                 ).get_default_values()
-            document = docutils.utils.new_document(self.filename, settings)
-            parser.parse(f, document)
+            self._document = docutils.utils.new_document(self.filename, settings)
+            parser.parse( f.read(), self._document )
             
-            print parser.document
-            
+    @property
+    def hardwareList(self):
+        # find the hardware list section
+        if not self._hwEntry:
+            for subdoc in self._document.traverse(condition=docutils.nodes.section):
+                if subdoc.attributes['ids'][0] == self._hwEntryIds:
+                    self._hwEntry = subdoc
+                    break
+    
+        # if still nothing
+        if not self._hwEntry:
+            print "failed to find hardware device list section named %s"%self._hwEntryIds
+            return None
+        
+        devList = {}
+        for section in self._hwEntry.traverse(condition=docutils.nodes.section):
+            if section.attributes['ids'][0] == self._hwEntryIds:
+                continue
+            devList[section.attributes['ids'][0]] = section.children[0].children[0]
+
+        return devList
+
--- nwdesign/tests/testReader.py
+++ nwdesign/tests/testReader.py
@@ -8,7 +8,7 @@ from restIO import *
 from restIO.restReader import *
 
 filename = "data/basicNw.rst"
-
+hwList = { "switch01": u'Switch01', "switch02": u'Switch02'}
 
 class Test(unittest.TestCase):
 
@@ -18,9 +18,9 @@ class Test(unittest.TestCase):
         self.assertEqual( r.filename, filename )
         pass
 
-    def testRead(self):
+    def testReadHardware(self):
         r = restReader( filename )
-        r.parseFile()
+        self.assertEqual( r.hardwareList, hwList )
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
